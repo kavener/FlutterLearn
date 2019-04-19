7ce66d3861ceb9701fc2ffd98b1623ffaefceaf8
kavener <271097513@qq.com>
2019-04-19 11:43:01 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
+
 // 应用程序入口，runApp功能即启动Flutter应用，接收的参数为Widget参数
 void main() => runApp(new MyApp());
 
@@ -25,7 +26,12 @@ class MyApp extends StatelessWidget {
       // 使用命名路由管理route，首先注册路由表
       routes: {
         "new_page": (context) => NewRoute(),
-        "cuper_page": (context) => CuoertinoTestRoute(),
+        // 使用新路由来学习 文本及样式
+        "text_page": (context) => NewText(),
+        // 学习 按钮
+        "button_page": (context) => NewButton(),
+        //  学习图片组件及ICON
+        'image_page': (context) => NewImage(),
       },
       // 应用首页路由
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
@@ -100,6 +106,29 @@ class _MyHomePageState extends State<MyHomePage> {
               textColor: Colors.blue,
               onPressed: () => Navigator.pushNamed(context, "cuper_page"),
             ),
+            // 添加文本及样式路由按钮
+            FlatButton(
+              child: Text('文本及样式',
+                style: TextStyle(
+                  background: new Paint()..color = Colors.yellow,
+                )
+              ),
+              
+              textColor: Colors.blue,
+              
+              onPressed: () => Navigator.pushNamed(context, "text_page"),
+            ),
+            // 添加按钮路由按钮，该button有阴影效果
+            RaisedButton(
+              child: Text('按钮'),
+              textColor: Colors.blue,
+              onPressed: () => Navigator.pushNamed(context, "button_page"),
+            ),
+            RaisedButton(
+              child: Text('图片组建及按钮'),
+              textColor: Colors.blue,
+              onPressed: () => Navigator.pushNamed(context, "image_page"),
+            ),
             // 通过english_words包随机显示一个英文单词
             new RandomWordsWidget(),
             // 打印文字的组件
@@ -129,18 +158,158 @@ class NewRoute extends StatelessWidget {
   }
 }
 
+class NewText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = new Scaffold(
+      appBar: AppBar(
+        title: Text('文本及样式'),
+      ),
+      body: new Center(
+          child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "文本居中对齐." * 7,
+            // 文本居中对齐
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "文本显示的最大行数." * 4,
+            // 文本显示的最大行数
+            maxLines: 1,
+            // 指定若文本超过一行的戒断方式，ellipsis 表示截断为省略号
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text(
+            "字体大小的缩放因子，类似于大小调节",
+            textScaleFactor: 1.5,
+          ),
+          Text("TextStyle用于指定文本显示的样式",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18,
+                height: 1.2,
+                fontFamily: "Courier",
+                background: new Paint()..color = Colors.yellow,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed,
+              )),
+          // 对不同片段设置不同的样式，可以不断嵌套设置
+          Text.rich(TextSpan(children: [
+            TextSpan(
+              text: 'Home ',
+            ),
+            TextSpan(
+              text: "https://flutterchina.club",
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+              // recognizer: _tapRecognizer,
+            ),
+          ])),
+          // 设置默认样式，这种样式是可以继承的
+          DefaultTextStyle(
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+            ),
+            textAlign: TextAlign.start,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("hello word!"),
+                Text('多行测试'),
+                // 尝试再次编辑文本样式，查看是否会覆盖默认样式
+                Text(
+                  '尝试再次编辑文本样式，查看是否会覆盖默认样式，结果：成功覆盖默认设置的样式',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 14.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
+    return scaffold;
+  }
+}
+
+class NewButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(title: Text('按钮')),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('RaiseButton'),
+              onPressed: (){},
+              ),
+            FlatButton(
+              child: Text("FlatButton"),
+              onPressed: (){},
+            ),
+            OutlineButton(
+              child: Text("OutlineButton"),
+              onPressed: (){},
+            ),
+            // 可点击的Icon
+            IconButton(
+              icon: Icon(Icons.thumb_up),
+              onPressed: (){},
+            ),
+            // 设置按钮样式，阴影风格真的不错。
+            RaisedButton(
+              child: Text("按钮样式"),
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              colorBrightness: Brightness.dark,
+              splashColor: Colors.grey,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              onPressed: (){},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(title:Text('图片及ICON')),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+
+      ),
+    );
+  }
+}
+
 class CuoertinoTestRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        middle: Text("Cupertino Demo"),),
+        middle: Text("Cupertino Demo"),
+      ),
       child: Center(
         child: CupertinoButton(
           color: CupertinoColors.activeBlue,
           child: Text("Press"),
-          onPressed: (){},
-          ),
+          onPressed: () {},
+        ),
       ),
     );
   }
